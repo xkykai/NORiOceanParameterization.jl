27986a672a2ce277845e7dd316f1a9aa944d333e
Xin Kai Lee <xinkai@mit.edu>
2025-11-11 14:34:58 -0500

    Add functions for predicting diffusive fluxes and dimensional boundary fluxes

--- src/ODEOperations/ODEOperations.jl
+++ src/ODEOperations/ODEOperations.jl
@@ -2,7 +2,8 @@ module ODEOperations
 
 export 
     predict_boundary_flux, predict_boundary_flux!,
-    predict_diffusivities, predict_diffusivities!
+    predict_diffusivities, predict_diffusivities!,
+    predict_diffusive_flux, predict_diffusive_boundary_flux_dimensional
 
 include("compute_fluxes_and_diffusivities.jl")
 end
--- src/ODEOperations/compute_fluxes_and_diffusivities.jl
+++ src/ODEOperations/compute_fluxes_and_diffusivities.jl
@@ -60,3 +60,52 @@ function predict_diffusivities!(νs, κs, Ris, ps)
     κs .= local_Ri_κ.(Ris, ps.ν_conv, ps.ν_shear, ps.Riᶜ, ps.ΔRi, ps.Pr_conv, ps.Pr_shear)
     return nothing
 end
+
+"""
+    predict_diffusive_flux(Ris, u_hat, v_hat, T_hat, S_hat, ps, params)
+
+Compute diffusive fluxes from Richardson numbers and normalized fields.
+Returns diffusive components of `uw, vw, wT, wS`.
+"""
+function predict_diffusive_flux(Ris, u_hat, v_hat, T_hat, S_hat, ps, params)
+    νs, κs = predict_diffusivities(Ris, ps)
+
+    ∂u∂z_hat = params.Dᶠ_hat * u_hat
+    ∂v∂z_hat = params.Dᶠ_hat * v_hat
+    ∂T∂z_hat = params.Dᶠ_hat * T_hat
+    ∂S∂z_hat = params.Dᶠ_hat * S_hat
+
+    uw_diffusive = -νs .* ∂u∂z_hat
+    vw_diffusive = -νs .* ∂v∂z_hat
+    wT_diffusive = -κs .* ∂T∂z_hat
+    wS_diffusive = -κs .* ∂S∂z_hat
+    return uw_diffusive, vw_diffusive, wT_diffusive, wS_diffusive
+end
+
+"""
+    predict_diffusive_boundary_flux_dimensional(Ris, u_hat, v_hat, T_hat, S_hat, ps, params)
+
+Compute total dimensional fluxes by combining diffusive and boundary flux contributions.
+Returns dimensional `uw, vw, wT, wS` flux arrays.
+"""
+function predict_diffusive_boundary_flux_dimensional(Ris, u_hat, v_hat, T_hat, S_hat, ps, params)
+    _uw_diffusive, _vw_diffusive, _wT_diffusive, _wS_diffusive = predict_diffusive_flux(Ris, u_hat, v_hat, T_hat, S_hat, ps, params)
+    _uw_boundary, _vw_boundary, _wT_boundary, _wS_boundary = predict_boundary_flux(params)
+
+    uw_diffusive = params.scaling.u.σ / params.H .* _uw_diffusive
+    vw_diffusive = params.scaling.v.σ / params.H .* _vw_diffusive
+    wT_diffusive = params.scaling.T.σ / params.H .* _wT_diffusive
+    wS_diffusive = params.scaling.S.σ / params.H .* _wS_diffusive
+
+    uw_boundary = inv(params.scaling.uw).(_uw_boundary)
+    vw_boundary = inv(params.scaling.vw).(_vw_boundary)
+    wT_boundary = inv(params.scaling.wT).(_wT_boundary)
+    wS_boundary = inv(params.scaling.wS).(_wS_boundary)
+
+    uw = uw_diffusive .+ uw_boundary
+    vw = vw_diffusive .+ vw_boundary
+    wT = wT_diffusive .+ wT_boundary
+    wS = wS_diffusive .+ wS_boundary
+
+    return uw, vw, wT, wS
+end
